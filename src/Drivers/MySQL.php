210158d3796c13249bbc5af680ef06139dcbac75
jatauje <jimmy.addiction@gmail.com>
2019-06-07 12:44:45 -0500

    fix character set position

--- src/Drivers/MySQL.php
+++ src/Drivers/MySQL.php
@@ -7,19 +7,19 @@ namespace Jinraynor1\TableImporter\Drivers;
 class MySQL extends AbstractDatabase
 {
 
-    public $local_in_file =  true;
+    public $local_in_file = true;
 
     public function optimizedInsert()
     {
 
         list($delimiter, $enclosure,) = $this->file->getCsvControl();
 
-        $local = $this->local_in_file?"LOCAL":"";
+        $local = $this->local_in_file ? "LOCAL" : "";
 
         $sql = '
-			LOAD DATA '.$local.' INFILE ' . $this->database->quote($this->file->getRealPath()) . '
-			CHARACTER SET BINARY
+			LOAD DATA ' . $local . ' INFILE ' . $this->database->quote($this->file->getRealPath()) . '			
 			IGNORE INTO TABLE ' . $this->quoteIdent('new_' . $this->table_name) . '
+			CHARACTER SET BINARY
 			FIELDS TERMINATED BY ' . $this->database->quote($delimiter) . '
 			OPTIONALLY ENCLOSED BY ' . $this->database->quote($enclosure) . '
 			ESCAPED BY ' . $this->database->quote($this->csv_escape_char);
